93c02b1684bd1d7f51bbbfb387eb8801dcc0a045
thanhtuandev <pro03tuan@gmail.com>
2025-03-04 14:59:52 +0700

    db

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -10,34 +10,32 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-
 @SpringBootApplication
 @RestController
-@RequestMapping("/api/products")
+// @RequestMapping("/api/products")
 public class DemoApplication {
 	// @RequestMapping("/")
 	// String home() {
-	// 	System.out.println("Hello");
 	// 	return "Hello World!";
 	// }
-	@GetMapping 
-    public String getAllProducts() {  
-        return "Lấy danh sách sản phẩm";  
-    }
-	@PostMapping  
-    public String createProduct() {  
-        return "Tạo sản phẩm mới";  
-    }  
+	// @GetMapping 
+    // public String getAllProducts() {  
+    //     return "Lấy danh sách sản phẩm";  
+    // }
+	// @PostMapping  
+    // public String createProduct() {  
+    //     return "Tạo sản phẩm mới";  
+    // }  
 
-    @PutMapping("/{id}")  
-    public String updateProduct(@PathVariable int id) {  
-        return "Cập nhật sản phẩm có ID: " + id;  
-    }  
+    // @PutMapping("/{id}")  
+    // public String updateProduct(@PathVariable int id) {  
+    //     return "Cập nhật sản phẩm có ID: " + id;  
+    // }  
 
-    @DeleteMapping("/{id}")  
-    public String deleteProduct(@PathVariable int id) {  
-        return "Xóa sản phẩm có ID: " + id;  
-    }  
+    // @DeleteMapping("/{id}")  
+    // public String deleteProduct(@PathVariable int id) {  
+    //     return "Xóa sản phẩm có ID: " + id;  
+    // }  
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
